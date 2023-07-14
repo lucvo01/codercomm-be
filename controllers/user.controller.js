e5487f72903d3c7b841ec2d84992a125f479bfba
lucvo01 <luke.syvo@gmail.com>
2023-07-14 15:57:28 +0000

    .

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,9 +1,30 @@
+const { sendResponse, AppError, catchAsync } = require("../helpers/utils");
 const User = require("../models/User");
 
 const userController = {};
 
-userController.register = async (req, res, next) => {
-  res.send("User Registration");
-};
+userController.register = catchAsync(async (req, res, next) => {
+
+    // Get data from request
+    const { name, email, password } = req.body;
+    // Validation
+    let user = await User.findOne({ email });
+   if(user) throw new AppError(400, "User already exists", "Registration Error")
+
+    // Process data
+    user = await User.create({name, email, password})
+
+    // Response
+    sendResponse(
+      
+      res,
+      200,
+      true,
+      { data: user },
+      null,
+      "Create User successful"
+    );
+  
+}) 
 
 module.exports = userController;
